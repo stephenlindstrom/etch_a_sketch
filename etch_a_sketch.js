8bef82891ffc01e41cf55dd422791b87bdaf62e5
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-24 10:01:23 -0700

    Create grid of squares using flexbox
    Each row is a flex container for squares that fill
    the horizontal space and also a flex item in the
    overall grid that has a column flex direction in
    order to fill all of the vertical space available.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -2,9 +2,12 @@ const GRID_SIZE = 4;
 const grid = document.querySelector("#container");
 
 for (let i=0; i<GRID_SIZE; i++) {
+    const row = document.createElement("div");
+    row.classList.add("flex-container");
+    grid.appendChild(row);
     for (let j=0; j<GRID_SIZE; j++) {
         const div = document.createElement("div");
         div.classList.add("default-square");
-        grid.appendChild(div);
+        row.appendChild(div);
     }
 }

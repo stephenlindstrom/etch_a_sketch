832f2b194fe227cc1878730f976f31dded112b4b
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-24 09:46:57 -0700

    Create grid size constant
    Allows size of grid to be more easily adjusted

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,7 +1,8 @@
+const GRID_SIZE = 4;
 const grid = document.querySelector("#container");
 
-for (let i=0; i<4; i++) {
-    for (let j=0; j<4; j++) {
+for (let i=0; i<GRID_SIZE; i++) {
+    for (let j=0; j<GRID_SIZE; j++) {
         const div = document.createElement("div");
         div.classList.add("default-square");
         grid.appendChild(div);

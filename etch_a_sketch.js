fa0b54e705879c9a289f5d6f31624ec011ade086
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-05-28 09:58:13 -0700

    Fill squares with random colors
    The red, green, and blue values in rgb function are
    assigned random values between 0-255

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,39 +1,45 @@
-const GRID_SIZE = 100;
-const grid = document.querySelector("#container");
-
-for (let i=0; i<GRID_SIZE; i++) {
-    const row = document.createElement("div");
-    row.classList.add("flex-container");
-    grid.appendChild(row);
-    for (let j=0; j<GRID_SIZE; j++) {
-        const div = document.createElement("div");
-        div.classList.add("default-square");
-        row.appendChild(div);
-    }
-}
-
-let mouseIsDown;
-grid.addEventListener("mousedown", () => {
-    mouseIsDown = true;
-    console.log(mouseIsDown);
-});
-grid.addEventListener("mouseup", () => {
-    mouseIsDown = false;
-});
-
-const squares = document.querySelectorAll(".default-square");
-squares.forEach((square) =>  {
-    square.addEventListener("mouseenter", () => {
-        if (mouseIsDown){
-            square.classList.add("filled-square");
-        };
-    });
-});
-
 button = document.querySelector('button');
 button.addEventListener('click', () => {
-    squares.forEach((square) => {
-        square.classList.remove("filled-square");
+    const rows = document.querySelectorAll(".flex-container");
+    rows.forEach((row) => {
+        row.remove();
+    }); 
+    const grid = document.querySelector("#container");
+    let gridSize = parseInt(prompt("What grid size (AxA)?"));
+    for (let i=0; i<gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("flex-container");
+        grid.appendChild(row);
+        for (let j=0; j<gridSize; j++) {
+            const div = document.createElement("div");
+            div.classList.add("default-square");
+            row.appendChild(div);
+        }
+    }
+
+    let mouseIsDown;
+    grid.addEventListener("mousedown", () => {
+        mouseIsDown = true;
+    });
+    grid.addEventListener("mouseup", () => {
+        mouseIsDown = false;
+    });
+
+    const squares = document.querySelectorAll(".default-square"); 
+    squares.forEach((square) =>  {
+        square.addEventListener("mouseenter", () => {
+            if (mouseIsDown){
+                const redValue = Math.floor(Math.random()*256);
+                const greenValue = Math.floor(Math.random()*256);
+                const blueValue = Math.floor(Math.random()*256);  
+                square.style.backgroundColor = `rgb(${redValue} ${greenValue} ${blueValue})`;
+            };
+        });
     });
 });
+
+
+
+
+
 
